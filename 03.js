7d541eb8139002a86259250e7944ec3ed9fa7488
Ninojakhveladze <Ninojakhveladze@users.noreply.github.com>
2020-05-21 02:33:30 +0400

    classwork 03

---
+++ 03.js
@@ -0,0 +1,60 @@
+//მიამატეთ თქვენს წინა მანქანების დავალებაში ფუნქცია
+//1) რომელიც ითვლის და აბრუნებს რიცხვთა მასივის საშუალო არითმეტიკულს და მისი გამოყენებით დაბეჭდეთ თქვენი მანქანების საშუალო ფასი შემდეგნაირი მესიჯით :
+//“Ჩვენს მარაგში არსებული მანქანების საშუალო ფასი არის : x” სადაც x რეალური რიცხვია.
+
+let Audi = {
+    mark: "აუდი",
+	color: "თეთრი",
+	model: "A4",
+	year: "2019",
+	power: "190",
+	price: 20000
+};
+
+let BMW = {
+    mark: "BMW",
+	color: "ლურჯი",
+	model: "A3",
+	year: "2018",
+	power: "190",
+	price: 30000
+};
+
+let TOYOTA = {
+    mark: "ტოიოტა",
+	color: "ვერცხლისფერი",
+	model: "A5",
+	year: "2018",
+	power: "200",
+	price: 40000
+};
+
+let HONDA = {
+    mark: "ჰონდა",
+	color: "ნაცრისფერი",
+	model: "A6",
+	year: "2018",
+	power: "150",
+	price: 10000
+};
+
+let cars = [];
+
+cars.push(Audi);
+cars.push(BMW);
+cars.push(TOYOTA);
+cars.push(HONDA);
+
+let prices = [HONDA.price, BMW.price, Audi.price, TOYOTA.price];
+
+let averageOfNumbers = averageOfNumbersInArray(prices);
+
+console.log(`Ჩვენს მარაგში არსებული მანქანების საშუალო ფასი არის: ${averageOfNumbers}`);
+
+function averageOfNumbersInArray(array) {
+	let sum = 0;
+	for (let i = 0; i < array.length; i++){
+		sum = sum + array[i];
+	}
+	return sum / array.length;
+  }
